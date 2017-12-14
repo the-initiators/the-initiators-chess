bce0aba319abc5d0a45f7af64627547105d8a854
Rebecca Sinkoff <rebecca.sinkoff@gmail.com>
2017-12-14 17:12:39 +0000

    Updated test/models/user_test.rb to add user email, password, and password confirmation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:user_name) { User.new(email: "test@test.com", password: "12345abcd", password_confirmation: "12345abcd") }
 
   it "must be valid" do
-    value(user).must_be :valid?
+    value(user_name).must_be :valid?
   end
 end
