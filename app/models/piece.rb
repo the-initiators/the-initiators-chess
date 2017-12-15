910d96c32aa34f46c1d34d723d19835a14f7e516
Rebecca Sinkoff <rebecca.sinkoff@gmail.com>
2017-12-15 23:26:17 +0000

    Added breakdown of the is_obstructed problem

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,2 +1,16 @@
 class Piece < ApplicationRecord
+    def is_obstructed
+
+    #  look to see if the spaces between the starting and ending points are open
+    # define starting position (x_position and y_position of current.piece)
+    # define ending position (x_position and y_position of where you want to go)
+    # look to see if there is an obstruction in the way (if any pieces.present?)
+    # if no obstruction
+        # allow move (return false)
+    # otherwise 
+        # return true
+       
+        
+    end
 end
+
