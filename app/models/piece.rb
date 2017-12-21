68b01ffe4156ae95dcbefa48cdd88132e6bef374
Rebecca Sinkoff <rebecca.sinkoff@gmail.com>
2017-12-21 17:50:16 +0000

    Added logic for diagonal obstruction

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,6 +2,7 @@ class Piece < ApplicationRecord
   def is_obstructed?(new_x, new_y)  
     horizontal_obstructed?(new_x)
     vertical_obstructed?(new_y)
+    diagonal_obstructed?(new_x, new_y)
   end 
   
   def horizontal_obstructed?(new_x) #Horizontal movement only
@@ -14,16 +15,9 @@ class Piece < ApplicationRecord
                 x_position:self.x_position).present?
   end
   
-  #  look to see if the spaces between the starting and ending points are open
-  # define starting position (x_position and y_position of current.piece)
-  # define ending position (x_position and y_position of where you want to go)
-  # look to see if there is an obstruction in the way (if any pieces.present?)
-  # if no obstruction
-  # allow move (return false)
-  # otherwise 
-  # return true
-  
-  
-
+  def diagonal_obstructed?(new_x, new_y)
+    Piece.where(y_position:(self.y_position + 1..new_y),
+                x_position:(self.x_position + 1..new_x)).present?
+  end
 end
 
