3ff0cc0517d35765d8f5172641405f76859f9992
Rebecca Sinkoff <rebecca.sinkoff@gmail.com>
2018-01-03 23:55:59 +0000

    potential fix for circleci issue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails/capybara'
+require 'factory_bot_rails'
 # require 'support/factory_bot'
 
 class ActiveSupport::TestCase
