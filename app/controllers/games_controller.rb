2da6f5e52bce79f1234e401a566328aa32b63317
Dorothy Eves <dorothyeves7@gmail.com>
2018-01-03 03:05:42 +0000

    gamelist try, still working on kinks and trying to display

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,10 +12,27 @@ class GamesController < ApplicationController
 
 
     def show
-        @game = Game.find(params[:id])
+        unless game.present?
+            return redirect_to root_path
+        end
     end
 
+    def update
+        if game.valid?
+            game.update_attributes games_params
+        end
+
+        return redirect_to game_path game
+    end
+
+
+
+
     private
+
+    def game
+        @game ||= Game.where(id: params[:id]).all
+    end
 
     def game_params
         params.require(:game).permit(:game_name, :white_player_id, :black_player_id)
