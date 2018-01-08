52589ef76b84e4e7fc7ab8fddf32b66bfd4ff1f8
falimo <falimo@yahoo.com>
2018-01-08 12:36:20 +0000

    here is the draft method for this model.  not sure how to test for it

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,4 +1,36 @@
 # frozen_string_literal: true
 
+
 class King < Piece
+
+# DETERMINE IF MOVE IS VALID
+# KING CAN MOVE 1 PLACE AT A TIME - UP, DOWN, LEFT, RIGHT
+# KING CAN MOVE 1 PLACE DIAGONALLY TOO
+# INVALID MOVE IF > 1 FROM CURRENT EITHER X OR Y COORD
+# CHECK IF OBSTRUCTED
+# OBSTRUCTED IF NEW POSITION HAVE PIECE OF SAME COLOR ON IT
+
+def valid_move? (move_to_x, move_to_y)
+
+	if (self.x_position - move_to_x).abs > 1 || (self.y_position - move_to_y).abs > 1
+		return false
+	if is_obstructed?(move_to_x, move_to_y, 1)
+		return false
+	else
+		return true
+	end
+end
+
+def is_obstructed? (move_to_x, move_to_y, z)
+
+	@move_to = @game.piece.where(x_position: move_to_x, y_position: move_to_y).take
+
+	if @move_to == nil
+		return false
+	if @move_to.color == self.color
+		return true
+	else
+		return false
+end
+
 end
