409d43feaa040fb38fca604d57b20d6a03eeb253
Dorothy Eves <dorothyeves7@gmail.com>
2018-01-10 22:37:08 +0000

    join game goes to the game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,13 +20,22 @@ class GamesController < ApplicationController
 
     def update
         @game = Game.find(params[:id])
+        @game.update_attributes(black_player_id: current_user.id)
+
         if game.valid? 
             game.update_attributes games_params
         end
 
         return redirect_to game_path(@game)
     end
+end
 
+def update_player
+    if @game.black_player_id.nil?
+        @game.update_attributes(black_player_id: current_user.id)
+    else
+        @game.update_attributes(white_player_id: current_user.id)
+    end
 end
 
 
