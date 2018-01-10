4e29846c2aad7478e31f33d7b159e7f2c1c3c6e3
falimo <falimo@yahoo.com>
2018-01-10 11:18:43 +0000

    revised code to pass tests and fix houndci-bot style issues

--- app/models/king.rb
+++ app/models/king.rb
@@ -3,44 +3,43 @@
 
 class King < Piece
 
-	# DETERMINE IF MOVE IS VALID
-	# KING CAN MOVE 1 PLACE AT A TIME - UP, DOWN, LEFT, RIGHT
-	# KING CAN MOVE 1 PLACE DIAGONALLY TOO
-	# INVALID MOVE IF > 1 FROM CURRENT EITHER X OR Y COORD
-	# CHECK IF OBSTRUCTED
-	# OBSTRUCTED IF NEW POSITION HAVE PIECE OF SAME COLOR ON IT
+# DETERMINE IF MOVE IS VALID
+# KING CAN MOVE 1 PLACE AT A TIME - UP, DOWN, LEFT, RIGHT
+# KING CAN MOVE 1 PLACE DIAGONALLY TOO
+# INVALID MOVE IF > 1 FROM CURRENT EITHER X OR Y COORD
+# CHECK IF OBSTRUCTED
+# OBSTRUCTED IF NEW POSITION HAVE PIECE OF SAME COLOR ON IT
 
-	def valid_move? (move_to_x, move_to_y)
+def valid_move? (move_to_x, move_to_y)
 
-		if (self.x_position - move_to_x).abs > 1 || (self.y_position - move_to_y).abs > 1
-			return false
-		end
-
-		if is_obstructed?(move_to_x, move_to_y)
-			return false
-		else
-			return true
-		end
-
+	if (self.x_position - move_to_x).abs > 1 || (self.y_position - move_to_y).abs > 1
+		return false
 	end
 
-	def is_obstructed? (move_to_x, move_to_y)
-
-		@move_to = King.where(x_position: move_to_x, y_position: move_to_y).take
-
-		if @move_to == nil
-			return false
-		end
-
-		if @move_to.color == self.color
-			return true
-		elsif @move_to.color != self.color
-			puts "Capture"
-			return false
-		else
-			return false
-		end
-
+	if is_obstructed?(move_to_x, move_to_y)
+		return false
+	else
+		return true
 	end
+
+end
+
+def is_obstructed? (move_to_x, move_to_y)
+
+	@move_to = King.where(x_position: move_to_x, y_position: move_to_y).take
+
+	if @move_to == nil
+		return false
+	end
+
+	if @move_to.color == self.color
+		return true
+	elsif @move_to.color != self.color
+		puts "Capture"
+		return false
+	else
+		return false
+	end
+end
 
 end
