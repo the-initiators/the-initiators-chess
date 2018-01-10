3bcbe336fe783e5095cbb1da1ea6b2c4008d09ee
Anson Melakayil <anson.melakayil@yahoo.com>
2018-01-10 18:05:44 +0000

    bishop valid move logic

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class Bishop < Piece
+
+	def bishop_valid_move?(x_move, y_move)
+
+		if (x_position - x_move).abs == (y.position - y).abs
+			return true
+		else
+			return false
+		end
+
+		if is_obstructed?(x_move, y_move) == true
+			return false
+		else
+			return true
+		end
+	end
+
 end
