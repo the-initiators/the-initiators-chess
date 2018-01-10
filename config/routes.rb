7c7264b5f68f53515e084caef47fe3d6151a7144
Dorothy Eves <dorothyeves7@gmail.com>
2018-01-10 05:25:57 +0000

    added email intro

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 	root 'landing_page#index'
 	resources :games, only: [:new, :create, :show]
+  resources :landing_page, only: [:index, :show, :open_games]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
