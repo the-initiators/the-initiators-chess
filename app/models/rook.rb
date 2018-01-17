3192321c2dbf28cc251aa13f369ff33169857fa3
Rebecca Sinkoff <rebecca.sinkoff@gmail.com>
2018-01-17 22:02:44 +0000

    Updated to address houndci comments

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -8,27 +8,27 @@ class Rook < Piece
   
   def rook_valid_move?(move_to_x, move_to_y)
 
-  	if (self.x_position - move_to_x).abs > (8 - self.x_position).abs || 
-  	   (self.y_position - move_to_y).abs > (8 - self.y_position).abs ||
-  	   ((self.x_position != move_to_x) && (self.y_position != move_to_y))
-  		return false
-  	end
-  	
-  	if is_obstructed?(move_to_x, move_to_y)
-  		return false
-  	else
-  		return true
-  	end
+    if (self.x_position - move_to_x).abs > (8 - self.x_position).abs || 
+       (self.y_position - move_to_y).abs > (8 - self.y_position).abs ||
+       ((self.x_position != move_to_x) && (self.y_position != move_to_y))
+      return false
+    end
+    
+    if is_obstructed?(move_to_x, move_to_y)
+      return false
+    else
+      return true
+    end
   end
 
-
   def is_obstructed?(move_to_x, move_to_y)
-  	@move_to = Piece.where(x_position: move_to_x, y_position: move_to_y, game_id: self.game_id).take
+    @move_to = Piece.where(x_position: move_to_x, y_position: move_to_y, 
+                           game_id: self.game_id).take
     if @move_to == nil
-  		return false
-  	elsif @move_to.color == self.color
-  		return true
-  	else #Piece is captured
+      return false
+    elsif @move_to.color == self.color
+      return true
+    else #Piece is captured
       return false
     end
   end
