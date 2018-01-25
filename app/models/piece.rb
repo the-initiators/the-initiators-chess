fd3ea63c49f1c4e67a26f1e7dd15ff2827aaed7d
Rebecca Sinkoff <rebecca.sinkoff@gmail.com>
2018-01-25 22:25:55 +0000

    Updated formatting

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -9,29 +9,20 @@ class Piece < ApplicationRecord
     invalid_move?(new_x.to_i, new_y.to_i)
   end
   
-  # This method is important and eventually, we will use it in the controller to handle moving pieces.
-  
-  # This move_to method should handle the following cases:
-  
-  # - Check to see if there is a piece in the location it’s moving to.
-  # - If there is a piece occupying the location, and it is the opposite color, remove the piece from the chess board. This can be done a few different ways.
-  #   - You could have a “status” flag on the piece that will be one of “onboard” or “captured”.
-  #   - You could set the piece’s x/y coordinates to nil
-  #   - You could delete the item from the database.
-  #   - Each solution has pros/cons.
-  # - If the piece is there and it’s the same color the move should fail - it should 
-  # either raise an error message or do nothing.
-  # - It should call update_attributes on the piece and change the piece’s x/y position.
   def move_to!(new_x, new_y)
     if !is_obstructed?(new_x, new_y)
       if landing_square_available?(new_x, new_y)
         # update piece attributes to new_x and new_y positions
         self.update_attributes(x_position: new_x, y_position: new_y)
-      elsif !landing_square_available?(new_x, new_y) && Piece.where(x_position: new_x, y_position: new_y, game_id: self.game_id).first.color == self.color
+      elsif !landing_square_available?(new_x, new_y) && Piece.where(
+            x_position: new_x, y_position: new_y, game_id: self.game_id)
+            .first.color == self.color
         # Invalid move
       else
-        Piece.where(x_position: new_x, y_position: new_y, game_id: self.game_id).first.update_attributes(x_position: nil, y_position: nil)
-        self.update_attributes(x_position: new_x, y_position: new_y, captured: true)
+        Piece.where(x_position: new_x, y_position: new_y, game_id: self.game_id)
+                    .first.update_attributes(x_position: nil, y_position: nil)
+        self.update_attributes(x_position: new_x, y_position: new_y, 
+                               captured: true)
         # Captures piece present
       end
     else 
@@ -57,12 +48,15 @@ class Piece < ApplicationRecord
 
   def diagonal_obstructed?(new_x, new_y)  #Diagonal movement only
     Piece.where(y_position:(self.y_position - 1...new_y),
-                x_position:(self.x_position - 1...new_x), game_id: self.game_id).present? &&
+                x_position:(self.x_position - 1...new_x), 
+                game_id: self.game_id).present? &&
     Piece.where(y_position:(self.y_position + 1...new_y),
-                x_position:(self.x_position + 1...new_x), game_id: self.game_id).present?
+                x_position:(self.x_position + 1...new_x), 
+                game_id: self.game_id).present?
   end
 
-  def invalid_move?(new_x, new_y) #Invalid movement (movement not horizontal, vertical, or diagonal)
+  def invalid_move?(new_x, new_y) 
+    #Invalid movement (movement not horizontal, vertical, or diagonal)
     if x_position == new_x && y_position == new_y
       return true
     elsif new_x < 1 || new_x > 8 || new_y < 1 || new_y > 8
